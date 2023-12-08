423475dc33d61f3686a9f902c75bc9c66454f80a
Jony Jas J <74784363+Jony-Jas@users.noreply.github.com>
2023-12-08 14:55:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+const NTRIPClient = require("./ntrip");
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const ntripClient = new NTRIPClient();
+
+wss.on("connection", (ws) => {
+  console.log("Client connected to WebSocket");
+
+  ws.on("message", (message) => {
+    const data = JSON.parse(message);
+
+    if (data.action === "connectToCaster") {
+      ntripClient.connectToCaster(ws);
+    } else if (data.action === "closeConnection") {
+      ntripClient.closeConnection();
+    } else if (data.action === "sendRequest") {
+      const data = {
+        baseStation: "data.baseStation",
+        mountPoint: "data.mountPoint",
+      };
+      ntripClient.sendRequestToCaster(data);
+    }
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected from WebSocket");
+  });
+});
+
+app.use(express.static("public"));
+
+server.listen(8080, () => {
+  console.log("Server is running on port 8080");
+});
---
+++ ntrip.js
@@ -0,0 +1,65 @@
+const net = require("net");
+
+class NTRIPClient {
+  constructor() {
+    this.tcpCasterIp = "127.0.0.1";
+    this.tcpCasterPort = 54321;
+    this.clientSocket = null;
+    this.ws = null;
+  }
+
+  connectToCaster(ws) {
+    this.ws = ws;
+    this.clientSocket = net.createConnection(
+      { host: this.tcpCasterIp, port: this.tcpCasterPort },
+      () => {
+        console.log(
+          `Connected to NTRIP caster at ${this.tcpCasterIp}:${this.tcpCasterPort}`
+        );
+      }
+    );
+
+    this.clientSocket.on("data", (data) => {
+      // ws.send(data.toString());
+      console.log(`Received from caster: ${data.toString()}`);
+    });
+
+    this.clientSocket.on("end", () => {
+      console.log("TCP Client shutting down.");
+    });
+
+    this.clientSocket.on("error", (err) => {
+      console.error(`Error: ${err.message}`);
+    });
+  }
+
+  sendRequestToCaster(RequestToSend) {
+    this.clientSocket.write(JSON.stringify(RequestToSend));
+  }
+
+  closeConnection() {
+    this.clientSocket.destroy();
+  }
+}
+
+module.exports = NTRIPClient;
+
+// Create Object:
+const ntripClient = new NTRIPClient();
+ntripClient.connectToCaster(null);
+
+//Sample Request Data
+const dataToSend = {
+  baseStation: "BS1",
+  mountPoint: "BS1-MP1",
+};
+
+//Send Rquest
+ntripClient.sendRequestToCaster(dataToSend);
+
+// Introduce a delay before closing the connection (in milliseconds)
+const delayBeforeClosing = 5000; // Adjust as needed
+setTimeout(() => {
+  console.log("Initiating connection closure.");
+  ntripClient.closeConnection();
+}, delayBeforeClosing);
